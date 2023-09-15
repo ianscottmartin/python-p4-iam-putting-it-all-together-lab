3dd6c63207f2d940a7264f12eb5b149af3e3e65c
Ian Scott Martin <ianscottmartin@gmail.com>
2023-09-14 19:15:54 -0600

    gettng started

--- server/models.py
+++ server/models.py
@@ -5,8 +5,12 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
-    pass
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
